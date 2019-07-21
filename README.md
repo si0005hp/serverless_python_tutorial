01b03f8cd2f681e16a967853fbe04794aecca220
si0005hp <si0005hp@gmail.com>
2019-07-21 12:36:42 +0900

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# 「Pythonで作るはじめてのサーバレスアプリケーション」サンプルコード
+
+「[Pythonで作るはじめてのサーバレスアプリケーション](https://www.amazon.co.jp/dp/B07JD6NDS7)」のサンプルコードです。
+
+- application: サーバレスBlogアプリケーション
+- serverless-bot: サーバレスBot
---
+++ application/flask_blog/models/entries.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, NumberAttribute, UTCDateTimeAttribute
+from flask_blog.lib.utils import is_production
+import os
+
+
+class Entry(Model):
+
+  class Meta:
+    table_name = "serverless_blog_entries"
+    region = 'ap-northeast-1'
+    if is_production():
+      aws_access_key_id = os.environ.get('SERVERLESS_AWS_ACCESS_KEY_ID')
+      aws_secret_access_key = os.environ.get('SERVERLESS_AWS_SECRERT_KEY')
+    else:
+      aws_access_key_id = 'AWS_ACEESS_KEY_ID'
+      aws_secret_access_key = 'AWS_SECRET_ACCESS_KEY'
+      host = "http://localhost:8000"
+
+  id = NumberAttribute(hash_key=True, null=False)
+  title = UnicodeAttribute(null=True)
+  text = UnicodeAttribute(null=True)
+  created_at = UTCDateTimeAttribute(default=datetime.now)
